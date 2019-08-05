e5670e663ba10b0ce0f74184b9ff525e996cc4fb
AgustinCragno <cragno.agustin@gmail.com>
2019-08-05 15:23:24 -0300

    Arregle un par de cosas que no estaban del todo bien

--- TPAFD.c
+++ TPAFD.c
@@ -21,11 +21,62 @@
 
 struct AFD{
     int estados[7];
-    char alfabeto[3];
+    char alfabeto[3];  // en el orden de las columnas
     int estadoInicial;
-    int estadosFinales[1];
-    int tablaDeTransicion[6][3]; //Filas estados, Columnas caracteres
+    int estadosFinales[2];
+    int tablaDeTransicion[7][3]; //Filas estados, Columnas caracteres
 };
+
+int length(int array[]){
+    return (int)( sizeof(array) / sizeof(int));
+}
+
+int lengthCadena(char array[]){
+    int i = 0;
+    while(array[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+int estadoEsFinal(struct AFD automata, int estado){
+
+    for(int i = 0; i < length(automata.estadosFinales) ; i++){
+        if( estado == automata.estadosFinales[i] ) return 1;
+    }
+
+    return 0;
+}
+
+int columnaDelCaracter(char caracter, struct AFD automata){
+    int i = 0;
+    while(automata.alfabeto[i] != '\0'){
+
+        if( caracter == automata.alfabeto[i] ){
+          return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+int pertenceAlLenguaje(struct AFD automata, char cadena[])
+{
+    int caracterActual;
+    int estadoActual = automata.estadoInicial;
+
+    for(int i = 0 ; i < lengthCadena(cadena) ; i++){
+
+        caracterActual = columnaDelCaracter(cadena[i], automata);printf("Caracter actual: %d\n", caracterActual);
+        if(caracterActual < 0) return 0;
+
+        estadoActual = automata.tablaDeTransicion[estadoActual][caracterActual];
+    }
+
+    return estadoEsFinal(automata, estadoActual);
+}
+
+
 
 int main(){
 
@@ -45,49 +96,9 @@ int main(){
 
     char cadena[] = "1+1=1";
 
-    printf("%d \t", pertenceAlLenguaje(automata, cadena) );
+    int pertenece = 9; pertenece = pertenceAlLenguaje(automata, cadena);
+
+    printf("\nPertenece: %d \n", pertenece );
 
     return 0;
-}
-
-int cantidadDeElementos(int array[]){
-    return sizeof(array);
-}
-
-int estadoEsFinal(struct AFD automata, int estado){
-
-    for(int i = 0; i < cantidadDeElementos(automata.estadosFinales) ; i++){
-        if( estado == automata.estadosFinales[i] ) return 1;
-    }
-
-    return 0;
-}
-
-int columnaDelCaracter(char caracter, struct AFD automata){
-    int i = 0;
-    while(automata.alfabeto[i] != '\0'){
-        if( caracter == automata.alfabeto[i] ){
-
-          return i;
-        }
-        i++;
-    }
-    return -1;
-}
-
-int pertenceAlLenguaje(struct AFD automata, char cadena[])
-{
-    int caracterActual;
-    int estadoActual = automata.estadoInicial;
-
-    for(int i = 0 ; i < cantidadDeElementos(cadena) + 1; i++){
-
-        caracterActual = columnaDelCaracter(cadena[i], automata);
-        if(caracterActual < 0) return 0;
-
-        estadoActual = automata.tablaDeTransicion[estadoActual][caracterActual];
-        //printf("%d ", estadoActual);
-    }
-
-    return estadoEsFinal(automata, estadoActual);
 }
