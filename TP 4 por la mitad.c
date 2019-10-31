612002a2c6dcb0164e4f2f5b2f6c56988297d576
AgustinCragno <cragno.agustin@gmail.com>
2019-10-31 15:52:51 -0300

    Intento de retrocederBuffer()

--- TP 4 por la mitad.c
+++ TP 4 por la mitad.c
@@ -19,14 +19,14 @@ typedef struct{
 } Tabla;
 
 //tabla de simbolos
-Tabla TS[100] = {{"algoritmo", ALGORITMO},
+Tabla TablaDeSimbolos[100] = {{"algoritmo", ALGORITMO},
 		 {"finAlgoritmo", FIN_ALGORITMO},
 		 {"leer", LEER},
 		 {"escribir", ESCRIBIR},
 		 {"si", SI},
 		 {"finSi", FIN_SI},
 		 {"variable", VARIABLE},
-		 {"¿", 999 }};
+		 {"Â¿", 999 }};
 
 char buffer[TAMLEX];
 TOKEN tokenActual;
@@ -35,7 +35,7 @@ int flagToken = 0;
 
 void Match(TOKEN t);
 TOKEN ProximoToken();
-int BuscarEnTs(char * id, Tabla * TS, TOKEN * t);
+int BuscarEnTs(char * id, Tabla * TablaDeSimbolos, TOKEN * t);
 TOKEN Scanner();
 int estadoFinal(int e);
 int columna(int c);
@@ -57,11 +57,13 @@ void ErrorLexico();
 void Condicion(); // no esta hecha
 void Identificador();  // no esta hecha
 
+FILE *archFuente;
+
 int main(){
-    FILE *archFuente = fopen("Ejemplo.txt", "r");
+	archFuente = fopen("Ejemplo.txt", "r");
     char nomArchi[TAMNOM];
     int l;
-    /***************************Se abre el Archivo Fuente******************/
+    /**********Se abre el Archivo Fuente*********/
     if ( archFuente == NULL ){
         printf("No se pudo abrir archivo fuente\n");
         return -1;//no pudo abrir archivo
@@ -71,38 +73,61 @@ int main(){
 }
 //funciones
 void Match(TOKEN t){
-    if( t != ProximoToken() ) ErrorSintactico();
+	printf("\n\n Intento de Match: %d", t);
+    if( t != ProximoToken() ){
+    	ErrorSintactico();
+	}
 }
 void ErrorLexico(){
     printf("Error Lexico\n");
 }
 
+void ErrorSintactico(){
+    printf("\n Error Sintactico");
+}
+
+
 TOKEN ProximoToken(){
     if ( !flagToken ){
         tokenActual = Scanner();
-        if ( tokenActual == ERRORLEXICO)ErrorLexico();
-        flagToken = 1;
+        
+        printf("\n Caracter leido: #%s# ", buffer);
+        printf("\n Token actual: %d", tokenActual);
+        
+        if ( tokenActual == ERRORLEXICO){
+			ErrorLexico();
+			flagToken = 1;
+		}
+        
         if ( tokenActual == IDENTIFICADOR ){
-            BuscarEnTs(buffer, TS, &tokenActual);
+        	
+            BuscarEnTs(buffer, TablaDeSimbolos, &tokenActual);
         }
     }return tokenActual;
 }
 
-int BuscarEnTs(char *id, Tabla * TS, TOKEN * t){
+int BuscarEnTs(char *id, Tabla * TablaDeSimbolos, TOKEN * t){
+	printf("\n Entra a Ts");
     /* Determina si un identificador esta en la TS */
     int i = 0;
-    while ( strcmp("¿", TS[i].identifi) ){
-        if ( !strcmp(id, TS[i].identifi) ){
-		*t = TS[i].t;
+    while ( strcmp("Â¿", TablaDeSimbolos[i].identifi) ){
+		if ( !strcmp(id, TablaDeSimbolos[i].identifi) ){
+			*t = TablaDeSimbolos[i].t;
+			printf("\n  Sale de TS con: %d", *t);
             return 1;
-        }i++;
-     }return 0;
+        }
+		i++;
+     }
+     
+     
+	 
+	 return 0;
 }
 //Identificador
 TOKEN Scanner(){
 	int tabla[NUMESTADOS][NUMCOLS] = {
 //   0   1  2  3  4  5  6   7  8  9  10 11 12 13
-//   L   D  +  -  [  ]  /   $  >  =  0 fdt   sp otro
+//   L   D  +  -  [  ]  /   $  >  =  0 fdt sp otro
  	{ 1, 3, 5, 6, 7, 8, 9, 10,11,12,13,14,0,15},
 	{ 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 	{15,15,15,15,15,15,15,15,15,15,15,15,15,15},
@@ -124,9 +149,9 @@ TOKEN Scanner(){
 	int col;
 	int estado = 0;
 	int i = 0;
-	char in;
+//	char in;
 	do {
- 		car = fgetc(in);
+ 		car = fgetc(archFuente);
  		col = columna(car);
  		estado = tabla[estado][col];
  		if ( col != 12 ) { //si es espacio no lo agrega al buffer
@@ -137,16 +162,18 @@ TOKEN Scanner(){
 
 		buffer[i] = '\0'; //completa la cadena
 		switch ( estado ){
- 				case 2 : // si es ID
-				 	if ( col != 12 ){ //si el carácter espureo no es un espacio
- 						ungetc(car, in); // lo retorna al flujo
- 						buffer[i-1] = '\0';
+ 			case 2 : // si es ID
+				if ( col != 12 ){ //si el carÃ¡cter espureo no es un espacio
+ 					//ungetc(car, archFuente); // lo retorna al flujo
+ 					//buffer[i-1] = '\0';
+ 					retrocederBuffer();
  				}
  				return IDENTIFICADOR;
  			case 4 :
 			  	if( col != 11 ) {
- 					ungetc(car, in);
- 					buffer[i-1] = '\0';
+ 					//ungetc(car, archFuente);
+ 					//buffer[i-1] = '\0';
+ 					retrocederBuffer();
  				}
  				return CONST_NUMERICA;
  			case 5 : return SUMA;
@@ -164,7 +191,25 @@ TOKEN Scanner(){
   return 0;
 }
 
-int estadoFinal(int e){
+int sizeBuffer(){
+	int i = 0;
+	while(buffer[i] != '\0'){
+		i++;
+	}
+	return i;
+}
+
+void retrocederBuffer(){
+	printf("	- Retroceder Buffer -	");
+	
+	int i = sizeBuffer();
+	while(i >= 0){
+		ungetc(buffer[i], archFuente);
+		i--;
+	}
+}
+
+int estadoFinal(int e){									// No, por ahi con lo que hicimos en el tp anterior??????????????????????????????????????????
     if ( e == 0 || e == 1 || e == 3 || e == 15 )
         return 0;
     return 1;
@@ -187,9 +232,7 @@ int columna(int c){
     return 13;
 }
 
-void ErrorSintactico(){
-    printf("\n Error Sintactico");
-}
+
 
 void Objetivo(){
     Programa();
@@ -226,7 +269,9 @@ void Sentencia(){
 }
 
 void Oracion(){
-    switch(ProximoToken()){
+	TOKEN t = ProximoToken();
+	retrocederBuffer();
+    switch(t){
         case VARIABLE:
             Declaracion();
         case LEER:
@@ -257,7 +302,7 @@ void Asignacion(){
     //Error Sintac tambien
 }
 
-void Lectura(){
+void Lectura(){printf("asdasdasd");
     Match(LEER);
     Match(FLECHA);
     Match(IDENTIFICADOR);
@@ -297,6 +342,7 @@ void Condicion(){
 void Identificador(){
 
 }
+
 void Operacion(){
     Match(CORCHETE_IZQ);
     Match(IDENTIFICADOR);
@@ -313,4 +359,3 @@ void Operador(){
     }
     //En Micro aca hace un else con Error Sintactico
 }
-
